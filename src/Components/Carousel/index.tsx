ff2c3fad219135bcd8272987799b29b1ea6db635
simecekmatej <simecekmatej@email.cz>
2023-06-03 13:55:26 +0200

    re organize repo

--- src/Components/Carousel/index.tsx
+++ src/Components/Carousel/index.tsx
@@ -1,31 +1,9 @@
 import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Flex, Image } from "@chakra-ui/react";
+
 import NavigationButton, { Orientation } from "./NavigationButton";
 
-type Item = {
-  id: number | string;
-  url: string;
-};
-
-const padItems = (items: Item[]): Item[] => {
-  if (items.length === 1)
-    return Array.from(Array(4).keys()).map((item) => ({
-      ...items[0],
-      id: item,
-    }));
-  else if (items.length < 4)
-    return items
-      .map<Item>((item) => ({ ...item, id: `padding-left-${item.id}` }))
-      .concat(items)
-      .concat(
-        items.map<Item>((item) => ({
-          ...item,
-          id: `padding-right-${item.id}`,
-        }))
-      );
-
-  return items;
-};
+import { Item, padItems } from "./utils";
 
 type CarouselProps = {
   items: Item[];
---
+++ src/Components/Carousel/utils.ts
@@ -0,0 +1,24 @@
+export type Item = {
+  id: number | string;
+  url: string;
+};
+
+export const padItems = (items: Item[]): Item[] => {
+  if (items.length === 1)
+    return Array.from(Array(4).keys()).map((item) => ({
+      ...items[0],
+      id: item,
+    }));
+  else if (items.length < 4)
+    return items
+      .map<Item>((item) => ({ ...item, id: `padding-left-${item.id}` }))
+      .concat(items)
+      .concat(
+        items.map<Item>((item) => ({
+          ...item,
+          id: `padding-right-${item.id}`,
+        }))
+      );
+
+  return items;
+};
