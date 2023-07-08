2179fa09a93938fbc89288dab723d8abd95f39c7
simecekmatej <simecekmatej@email.cz>
2023-07-08 08:34:22 +0200

    add photos

--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,11 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 import { BackgroundEffect, Carousel, Navigation } from "./Components";
 
 const Dashboard = () => (
   <Box w="100vw" h="100vh" overflow="hidden auto" position="relative">
     <Navigation />
-    <Carousel />
+    <Flex justify="center" pt="20px">
+      <Box w="80%" maxW="1000px">
+        <Carousel />
+      </Box>
+    </Flex>
     <BackgroundEffect />
   </Box>
 );
